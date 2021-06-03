e60faba4eae59d85943650f9d6068537659088d9
Israel Flores <israelflores@byui.edu>
2021-06-02 21:26:29 -0600

    week 7 team project

---
+++ w07/comments.js
@@ -0,0 +1,13 @@
+// const newComment = {
+//     name: hikeName,
+//     date: new Date(),
+//     content: comment
+//   };
+
+export default class Comments {
+    constructor(name, content) {
+        this.name = name;
+        this.content - content;
+        this.date = new Date();
+    }
+}
