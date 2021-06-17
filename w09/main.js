8994c7dd5302a234d2015a24d9ac92fc7102862f
israelflores95 <israelflores@byui.edu>
2021-06-16 20:12:42 -0600

    some stretch

--- w09/main.js
+++ w09/main.js
@@ -1,10 +1,19 @@
 const keyboardDown = window.addEventListener('keydown', keyDown);
+let moveBox = 0;
 
 function keyDown(keyboardDown) {
     const audio = document.querySelector(`audio[data-key="${keyboardDown.keyCode}"]`);
 
     const key = document.querySelector(`.key[data-key="${keyboardDown.keyCode}"]`);
     key.classList.add('playing');
+
+    if(moveBox === 100){
+        moveBox = 0;
+    }
+
+    key.style.position = 'relative';
+    key.style.top = `${moveBox}px`;
+    moveBox += 10;
 
     if(!audio) return; //only assigned keys will work
 
@@ -19,5 +28,3 @@ function transitionBack(keyboardDown) {
 let key = document.querySelector(`[data-key="${keyboardDown.keyCode}"`);
 key.classList.remove('playing');
 }
-
-
