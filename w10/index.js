e6d2a369b3162fa09bd5df7e20c673e0b9b6f5a9
Israel Flores <israelflores@byui.edu>
2021-06-23 20:09:46 -0600

    for lautero

--- w10/index.js
+++ w10/index.js
@@ -5,4 +5,6 @@ import QuakesController from './quakescontroller.js';
 const baseUrl ='https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02';
 
 
-const controller = new QuakesController().init;
+const controller = new QuakesController().init();
+
+console.log(controller);
