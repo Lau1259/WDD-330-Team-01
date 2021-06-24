750e2fc6a4190d96c0ab90d3a6335df0670f879d
etrix06 <alc18005@byui.edu>
2021-06-23 19:47:15 -0700

    ch

--- w10/index.js
+++ w10/index.js
@@ -1,10 +1,5 @@
 //import { getJSON, getLocation } from 'utilities.js';
 import QuakesController from './quakescontroller.js';
 
-
-const baseUrl ='https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02';
-
-
-const controller = new QuakesController().init();
-
-console.log(controller);
+ const myQuakesController = new QuakesController('#quakeList');
+ myQuakesController.getQuakesByRadius();
