1464b0570d08378095228ce88cff7d4791f2a348
Lau1259 <62459638+Lau1259@users.noreply.github.com>
2021-06-30 22:22:06 -0300

    Added the base server code

--- w10/index.js
+++
@@ -1,5 +0,0 @@
-import QuakesController from './quakescontroller.js';
-
-const myQuakesController = new QuakesController('#quakeList');
-myQuakesController.init();
-myQuakesController.getQuakesByRadius();
--- w10/js/quake.js
+++ w10/js/quake.js
@@ -3,12 +3,11 @@ import { getJSON } from './utilities.js';
 export default class Quake {
   constructor() {
     this.baseUrl =
-    'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson';
-    // this is where we will store the last batch of retrieved quakes in the model.  I don't always do this...in this case the api doesn't have an endpoint to request one quake.
+      'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson';
+    // store the last batch of retrieved quakes in the model.  I don't always do this...in this case the api doesn't have an endpoint to request one quake.
     this._quakes = [];
   }
   async getEarthQuakesByRadius(position, radius = 100) {
-    // use the getJSON function and the position provided to build out the correct URL to get the data we need.  Store it into this._quakes, then return it
     this._quakes = await getJSON(
       this.baseUrl +
         `&starttime=2019-01-01&endtime=2019-03-02&latitude=${
@@ -16,11 +15,8 @@ export default class Quake {
         }&longitude=${position.lon}&maxradiuskm=${radius}`
     );
     return this._quakes;
-
   }
   getQuakeById(id) {
-    // filter this._quakes for the record identified by id and return it
     return this._quakes.features.filter(item => item.id === id)[0];
   }
 }
-    
